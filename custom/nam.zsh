979606ee1eaedc5d01d4030000483d86ddcb9e59
Nam Le <namle117@gmail.com>
2014-05-13 13:11:10 -0700

    adding my own custom configs

---
+++ custom/nam.zsh
@@ -0,0 +1,8 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+
+echo '====== Nam`s Configs ====='
+
+
+code=~/Development/code
---
+++ custom/plugins/uxdev/uxdev.plugin.zsh
@@ -0,0 +1,10 @@
+# export PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin"
+
+echo '------ UXDev Plugin ------'
+
+alias sub="subl"
+alias web="python -m SimpleHTTPServer 8000"
+
+export PATH="$PATH:$HOME/bin:$HOME/.rbenv/bin"
+
+export EDITOR='subl -w'
